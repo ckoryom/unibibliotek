c168f8e10e5af24357df8a221e8787d55d8d3726
skimov <stas_kimoff@mail.ru>
2014-04-05 20:33:26 +0200

    Result screen should show view with titles.
    But something is apparently wrong. Falls on webservice.connect

--- UniBibliotek/src/com/project/unibibliotek/ResultsActivity.java
+++ UniBibliotek/src/com/project/unibibliotek/ResultsActivity.java
@@ -1,20 +1,27 @@
 package com.project.unibibliotek;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
-import com.project.unibibliotek.logic.WebService;
-import com.project.unibibliotek.model.Book;
-
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ResultsActivity extends ActionBarActivity {
+import com.project.unibibliotek.logic.WebService;
+import com.project.unibibliotek.model.Book;
+
+public class ResultsActivity extends Activity {
 	
 	public List<Book> booksList;
 	public WebService librarian;
@@ -32,8 +39,33 @@ public class ResultsActivity extends ActionBarActivity {
         
         //TODO: request results and display them in a list
         librarian = new WebService();
-        booksList = librarian.search("",bookTitleToSearch);
-        //In progress..
+        try {
+			librarian.setUrl(new URL("http://ubz-primo-test.hosted.exlibrisgroup.com/PrimoWebServices/services/tags"));
+		} catch (MalformedURLException e) { e.printStackTrace(); }
+        librarian.connect();
+        booksList = librarian.search("39UBZ",bookTitleToSearch);
+        String[] booksArray = new String[booksList.size()];
+        for (int i=0; i<booksList.size(); i++)
+        {
+        	Book book = booksList.get(i);
+        	booksArray[i] = book.getTitle();
+        }
+        //booksList.toArray(booksArray);
+        
+        ListView resultLV = (ListView) findViewById(R.id.resultListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, booksArray);
+        resultLV.setAdapter(arrayAdapter);
+        
+        resultLV.setOnItemClickListener(new OnItemClickListener() 
+        {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+        });
 	}
 
 	@Override
